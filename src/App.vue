c8e45908fda4f3cf24dcf555895fa78ce18d9173
ming <xiemingzhi@gmail.com>
2020-01-20 17:13:38 +0800

    Add bottom-bar

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,13 @@
         <!-- <TodoList /> -->
         <router-view />
       </md-app-content>
+
     </md-app>
+    <md-bottom-bar md-sync-route>
+      <md-bottom-bar-item to="/components/bottom-bar" exact md-label="Home" md-icon="home"></md-bottom-bar-item>
+      <md-bottom-bar-item to="/components/bottom-bar/posts" md-label="Posts" md-icon="/assets/icon-whatshot.svg"></md-bottom-bar-item>
+      <md-bottom-bar-item to="/components/bottom-bar/favorites" md-label="Favorites" md-icon="favorite"></md-bottom-bar-item>
+    </md-bottom-bar>
   </div>
 </template>
 
